e0edc3723fd9242e87fbaeefe68903d106d5e46a
M3 <34512995+Grublux@users.noreply.github.com>
2024-01-05 13:49:27 +0000

    Second Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed on", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let chooseWho = who[Math.floor(Math.random() * who.length)];
+  let chooseAction = action[Math.floor(Math.random() * action.length)];
+  let chooseWhat = what[Math.floor(Math.random() * what.length)];
+  let chooseWhen = when[Math.floor(Math.random() * when.length)];
+
+  document.getElementById("excuse").innerHTML =
+    chooseWho + " " + chooseAction + " " + chooseWhat + " " + chooseWhen;
+
+  console.log();
 };
